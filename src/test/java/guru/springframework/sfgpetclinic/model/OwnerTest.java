eca5cc689eced3ff2b5a289ef47ea6500627187b
l.galati <galatil.luca@gmail.com>
2021-11-23 11:09:30 +0100

    third lesson: dependent assertions

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,27 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerTest {
+
+    @Test
+    void dependentAssertions() {
+
+        Owner owner = new Owner(1L, "Joe", "Buck");
+        owner.setCity("Key West");
+        owner.setTelephone("1231231234");
+
+        assertAll("Properties Test",
+                () -> assertAll("Person Properties",
+                        () -> assertEquals("Joe", owner.getFirstName(), "First Name did not match"),
+                        () -> assertEquals("Buck", owner.getLastName())
+                ),
+                () -> assertAll("Owner Properties",
+                        () -> assertEquals("Key West", owner.getCity(), "City did not match"),
+                        () -> assertEquals("1231231234", owner.getTelephone())
+                )
+        );
+    }
+}
